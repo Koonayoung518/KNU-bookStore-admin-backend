ab5d6dec870b6c13d069f4620db3763d8da0449a
Koonayoung518 <koony000415@gmail.com>
2022-11-08 23:19:59 +0900

    refactor(application.yml): flyway 수정

--- src/main/resources/db/migration/mysql/V202211022__alter_character_set.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE admin CONVERT TO character SET utf8;
-ALTER TABLE book CONVERT TO character SET utf8;
-ALTER TABLE bookstore_info CONVERT TO character SET utf8;
-ALTER TABLE history CONVERT TO character SET utf8;
-ALTER TABLE sell CONVERT TO character SET utf8;
